b2d84185c21dac50631216dfc51566dd2361596f
nickJ <nicknotjustin@gmail.com>
2019-01-15 11:10:42 -0600

    Added Authentication Pathing and Exports to config. Also insured dotenv was installed.

--- config.js
+++ config.js
@@ -5,5 +5,7 @@ require('dotenv').config();
 module.exports = {
   PORT: process.env.PORT || 8080,
   MONGODB_URI: process.env.MONGODB_URI || 'mongodb://localhost/noteful',
-  TEST_MONGODB_URI: process.env.TEST_MONGODB_URI || 'mongodb://localhost/noteful-test'
+  TEST_MONGODB_URI: process.env.TEST_MONGODB_URI || 'mongodb://localhost/noteful-test',
+  JWT_SECRET: process.env.JWT_SECRET,
+  JWT_EXPIRY: process.env.JWT_EXPIRY || '7d'
 };
